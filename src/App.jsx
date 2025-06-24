e3ca5f8e1462a0c29b954e5a2eefe90369fcc401
THOMASUKUTTY REJI <154000604+thomasukutty07@users.noreply.github.com>
2025-06-24 11:06:55 +0530

    implemented cocktails

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
+import About from "./components/About";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -11,6 +13,8 @@ const App = () => {
     <main>
       <NavBar />
       <Hero />
+      <Cocktails />
+      <About />
     </main>
   );
 };
